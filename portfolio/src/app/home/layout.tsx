69fc27a5a608afb7a70912e8b3035c0e6b393c7b
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-08-12 19:40:52 -0600

    🚧 Added template for /home

---
+++ portfolio/src/app/home/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Guti's Portfolio",
+  description: "Diego Gutierrez' Portfolio Home",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="overflow-hidden">
+        {children}
+      </body>
+    </html>
+  );
+}
