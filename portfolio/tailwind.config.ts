732f832c12fb36a8febdd2c2092fa6485e2207ef
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-08-12 19:39:44 -0600

    🎨 Added personal styling

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -12,6 +12,23 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'monitor-lines': 'repeating-linear-gradient(transparent, #00000044 0.5em)'
+      },
+      boxShadow: {
+        'monitor': '5px 0 18rem #031e11',
+        'monitor-lines': '5px 5px 25px #14fdce20',
+        'monitor-shadow': 'inset 0 0 18rem black, inset 0 0 3rem black, 0 0 10rem black;'
+      },
+      keyframes: {
+        dots: {
+          '0%, 20%': { content: "''" },
+          '40%': { content: "'.'" },
+          '60%': { content: "'..'" },
+          '80%, 100%': { content: "'...'" },
+        }
+      },
+      animation: {
+        dots: 'dots 1.5s steps(3, end) infinite',
       },
     },
   },
