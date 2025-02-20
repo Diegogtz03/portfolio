04c40fc3486c7f2237d4b16491e2096bac7f5ca0
Diego Gtz <diego_gtz_t@hotmail.com>
2025-02-19 20:55:24 -0600

    Home page

--- portfolio/src/app/home/layout.tsx
+++ portfolio/src/app/home/layout.tsx
@@ -1,19 +1,32 @@
 import type { Metadata } from "next";
+import { Header } from "@/components/global/Header";
+import { ThemeProvider } from "@/components/global/theme-provider";
+import { helveticaFont } from "@/helper/helveticaFont";
+import NavBar from "@/components/global/NavBar";
 
 export const metadata: Metadata = {
-  title: "Guti's Portfolio",
+  title: "Home",
   description: "Diego Gutierrez' Portfolio Home",
 };
 
-export default function RootLayout({
+export default function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className="bg-dark-bg">
-        {children}
+      <body className={`${helveticaFont.className} bg-light-bg`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+          <NavBar />
+        </ThemeProvider>
       </body>
     </html>
   );
--- portfolio/src/app/home/page.tsx
+++ portfolio/src/app/home/page.tsx
@@ -1,21 +1,19 @@
-import { Girassol } from "next/font/google";
-import { NavigationMenu } from "@/components/NavigationMenu/NavigationMenu";
+"use client";
 
-const girassol = Girassol({ weight: "400", subsets: ["latin"] });
+import HomeText from "@/components/home/HomeText/HomeText";
+import FloatingItems from "@/components/home/FloatingItems/FloatingItems";
+
+import { useRef } from "react";
 
 export default function Home() {
+  const containerRef = useRef<any>();
+
   return (
-    <main className="overflow-hidden bg-[#1a1a1a] w-screen h-screen">
-      <NavigationMenu />
-
-      <div className="w-fit h-fit absolute top-[50%] right-[50%] translate-x-1/2 -translate-y-1/2">
-        <p className={`${girassol.className} text-12xl text-white select-none`}>
-          DIEGO
-        </p>
+    <main className="overflow-hidden w-screen h-screen" ref={containerRef}>
+      <FloatingItems containerRef={containerRef} />
+      <div className="w-fit h-fit absolute top-[50%] right-[50%] translate-x-1/2 -translate-y-1/2 z-20">
+        <HomeText />
       </div>
-
-      {/* <LiveMusicDisplay /> */}
-      {/* <EmailBtn /> */}
     </main>
   );
 }
